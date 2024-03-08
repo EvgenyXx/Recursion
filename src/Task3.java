239de10327429ea09731bdf008313e914e9ef0ed
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-03-08 17:46:25 +0300

    рекурсия где переворачивается массив

--- src/Task3.java
+++ src/Task3.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Random  random = new Random();
+       int [] array = new int[]{2,5,7,9,3,4};
 
+
+       ReverseArray(array,0 );
+
+
+
+
+    }
+    public static void ReverseArray (int [] arr , int index ) {
+        if (index < arr.length){
+
+            ReverseArray(arr,index +1);
+            System.out.println(arr[index]);
+        }
     }
 }
