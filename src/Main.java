61ba1fc52032d37b3c7002d47058fcfc0b514829
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-03-08 14:31:25 +0300

    задача 1 .Вывод натуральных чисел

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //  Задайте значения M и N. Напишите программу, которая выведет все
+        //  натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+        int m = 1;
+        int n = 10;
 
+        System.out.print("Натуральные числа от " + m + " до " + n + "\n"  );
+        printNaturalNumbers(m, n);
+
+
+    }
+    public static  void printNaturalNumbers(int m , int n) {
+        if (m <= n) {
+            System.out.println(m);
+            printNaturalNumbers(m + 1, n);
+        }
     }
 }
