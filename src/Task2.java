23779cc7df9f89da776de8366e7c49afe1c332ed
Evgeny Pavlov <Evgenypavlov666@yandex.ru>
2024-03-08 16:06:31 +0300

    функция Аккермана

--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,21 @@
 public class Task2 {
     public static void main(String[] args) {
-    // Напишите программу вычисления функции Аккермана с помощью рекурсии.
+    // Напишите программу вычисления  Аккермана  помощью рекурсии.
         // Даны два неотрицательных числа m и n.
+        int m = 2;
+        int n = 1;
+        int result = ackermann(m, n);
+        System.out.println("Результат функции Аккермана для m = " + m + " и n = " + n + " равен " + result);
+
+
+    }
+    public static int ackermann(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if (m > 0 && n == 0) {
+            return ackermann(m - 1, 1);
+        } else {
+            return ackermann(m - 1, ackermann(m, n - 1));
+        }
     }
 }
